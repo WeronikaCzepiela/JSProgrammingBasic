272bd8ee2963856b84e67833ccb0da7b79b64886
marcin.czepiela <marcin.czepiela@humanit.group>
2023-06-05 20:42:18 +0200

    Add base files layout for longest-common-prefix

---
+++ algorithms/longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,6 @@
+export function longestCommonPrefix(strs) {
+
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(longestCommonPrefix(["dog", "racecar", "car"]))
---
+++ algorithms/longest-common-prefix/longest-common-prefix.test.js
@@ -0,0 +1,3 @@
+describe('roman to integer', () => {
+    // TODO
+})
