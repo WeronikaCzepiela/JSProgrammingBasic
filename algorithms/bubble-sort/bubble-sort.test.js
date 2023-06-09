95ebea64d3726500646e8e8bc2b598c1b52f1993
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-09 16:40:52 +0200

    Add tests for bubble sort algorithm

---
+++ algorithms/bubble-sort/bubble-sort.test.js
@@ -0,0 +1,13 @@
+import {bubbleSort} from "../bubble-sort/bubbleSort"
+
+describe('bubble sort', () => {
+    it.each([
+        [[1, 6, 2, 3, 9, 5, 2, 6, 11], [1, 2, 2, 3, 5, 6, 6, 9, 11]],
+        [[1, 6, 2, 22, 3, 9, 5, -1, 2, 40, 6, 11], [-1, 1, 2, 2, 3, 5, 6, 6, 9, 11, 22, 40]],
+        [[], []],
+    ])('should sort numbers in the array', (data, expected) => {
+        const result = bubbleSort(data)
+
+        expect(result).toEqual(expected)
+    })
+})
