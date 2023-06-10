721208918e244b9e4aecd2c7b252d13a31e927f9
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-10 15:40:49 +0200

    Try to write an algorithm to longest-common-prefix

--- algorithms/longest-common-prefix/longest-common-prefix.js
+++ algorithms/longest-common-prefix/longest-common-prefix.js
@@ -1,6 +1,23 @@
-export function longestCommonPrefix(strs) {
+const longestCommonPrefix = (strs) => {
+    let prefix = strs[0].substring(0, 1)
 
+    for (let i = 1; i < strs.length; i++) {
+        for (let j = 1; j < strs[0].length; j++) {
+            if ((strs[i].startsWith(prefix)) && (strs[i+1].startsWith(prefix))) {
+                prefix = strs[0].substring(0, j-1)
+                // console.log(prefix)
+            }
+        }
+    }
+
+    return prefix
 }
 
-console.log(longestCommonPrefix(["flower", "flow", "flight"]))
-console.log(longestCommonPrefix(["dog", "racecar", "car"]))
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])) //fl
+console.log(longestCommonPrefix(["dog", "racecar", "car"])) //[]
+
+
+// export function longestCommonPrefix(strs) {
+//
+// }
