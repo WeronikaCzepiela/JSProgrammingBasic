579858ef3ad5c27828913315dd736c2ae17d38cd
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-11 11:46:53 +0200

    Add an solution to longest-common-prefix

--- algorithms/longest-common-prefix/longest-common-prefix.js
+++ algorithms/longest-common-prefix/longest-common-prefix.js
@@ -1,11 +1,11 @@
 const longestCommonPrefix = (strs) => {
-    let prefix = strs[0].substring(0, 1)
+    let prefix = []
 
     for (let i = 1; i < strs.length; i++) {
+        if (i+1 === strs.length) {break}
         for (let j = 1; j < strs[0].length; j++) {
-            if ((strs[i].startsWith(prefix)) && (strs[i+1].startsWith(prefix))) {
-                prefix = strs[0].substring(0, j-1)
-                // console.log(prefix)
+            if ((strs[i].startsWith(strs[0].substring(0, j))) && (strs[i+1].startsWith(strs[0].substring(0, j)))) {
+                prefix = strs[0].substring(0, j)
             }
         }
     }
