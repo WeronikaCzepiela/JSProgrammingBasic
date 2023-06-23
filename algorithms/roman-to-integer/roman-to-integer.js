8ba2c4a5ed6e115036912dc24513919e081cd7f9
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-23 21:26:04 +0200

    Change string to number in an array and add first version of function

--- algorithms/roman-to-integer/roman-to-integer.js
+++ algorithms/roman-to-integer/roman-to-integer.js
@@ -1,9 +1,51 @@
-export function romanToInt(s) {
-  // ...
-}
+const romanToInt = (s) => {
+  //Zmiaana znaków w tablicy z rzysmkich na liczby
+  const array = s.split("");
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "I") {
+      array.splice(i, 1, 1);
+    }
+    if (array[i] === "V") {
+      array.splice(i, 1, 5);
+    }
+    if (array[i] === "X") {
+      array.splice(i, 1, 10);
+    }
+    if (array[i] === "L") {
+      array.splice(i, 1, 50);
+    }
+    if (array[i] === "C") {
+      array.splice(i, 1, 100);
+    }
+    if (array[i] === "M") {
+      array.splice(i, 1, 1000);
+    }
+  }
 
-console.log(romanToInt('III')) // 3
-console.log(romanToInt('IV')) // 4
-console.log(romanToInt('IX')) // 9
-console.log(romanToInt('LVIII')) //50
-console.log(romanToInt('MCMXCIV')) // 1994
+  //Dodawanie
+  let a = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i + 1 < array.length) {
+      if (array[i] < array[i + 1]) {
+        a = a + array[i + 1] - array[i];
+        i++;
+      }
+      if (array[i] === array[i + 1]) {
+        a = a + array[i + 1] + array[i];
+        i++;
+      } else {
+        a = a + array[i];
+      }
+    } else {
+      a = a + array[i];
+    }
+  }
+
+  return a;
+};
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("IV")); // 4
+console.log(romanToInt("IX")); // 9
+console.log(romanToInt("LVIII")); //58
+console.log(romanToInt("MCMXCIV")); // 1994
