aaa563634760125da6b55c482461ffe17cc97669
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-06-27 15:04:14 +0200

    Change solution for longest common prefix

--- algorithms/longest-common-prefix/longest-common-prefix.js
+++ algorithms/longest-common-prefix/longest-common-prefix.js
@@ -1,22 +1,31 @@
 const longestCommonPrefix = (strs) => {
-    let prefix = []
+  let prefix = "";
 
-    for (let i = 1; i < strs.length; i++) {
-        if (i+1 === strs.length) {break}
-        for (let j = 1; j < strs[0].length; j++) {
-            if ((strs[i].startsWith(strs[0].substring(0, j))) && (strs[i+1].startsWith(strs[0].substring(0, j)))) {
-                prefix = strs[0].substring(0, j)
-            }
+  if (strs.length === 1) {
+    prefix = strs[0];
+  } else {
+    for (let i = 0; i < strs.length; i++) {
+      if (i + 1 === strs.length) {
+        break;
+      }
+      for (let j = 1; j < strs[0].length; j++) {
+        if (
+          strs[i].startsWith(strs[0].substring(0, j)) &&
+          strs[i + 1].startsWith(strs[0].substring(0, j))
+        ) {
+          prefix = strs[0].substring(0, j);
         }
+      }
     }
+  }
 
-    return prefix
-}
+  return prefix;
+};
 
-
-console.log(longestCommonPrefix(["flower", "flow", "flight"])) //fl
-console.log(longestCommonPrefix(["dog", "racecar", "car"])) //[]
-
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); //fl
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); //''
+console.log(longestCommonPrefix(["dog"])); //dog
+console.log(longestCommonPrefix(["ab", "a"])); //a
 
 // export function longestCommonPrefix(strs) {
 //
