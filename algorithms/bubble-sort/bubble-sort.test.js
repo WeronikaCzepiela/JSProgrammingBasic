ae2bf89d6e3913a2151a154f501e9f629c5233ae
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-02-14 21:11:48 +0100

    add export to function

--- algorithms/bubble-sort/bubble-sort.test.js
+++ algorithms/bubble-sort/bubble-sort.test.js
@@ -1,13 +1,19 @@
-import {bubbleSort} from "../bubble-sort/bubbleSort"
+import { bubbleSort } from "./bubbleSort";
 
-describe('bubble sort', () => {
-    it.each([
-        [[1, 6, 2, 3, 9, 5, 2, 6, 11], [1, 2, 2, 3, 5, 6, 6, 9, 11]],
-        [[1, 6, 2, 22, 3, 9, 5, -1, 2, 40, 6, 11], [-1, 1, 2, 2, 3, 5, 6, 6, 9, 11, 22, 40]],
-        [[], []],
-    ])('should sort numbers in the array', (data, expected) => {
-        const result = bubbleSort(data)
+describe("bubble sort", () => {
+  it.each([
+    [
+      [1, 6, 2, 3, 9, 5, 2, 6, 11],
+      [1, 2, 2, 3, 5, 6, 6, 9, 11],
+    ],
+    [
+      [1, 6, 2, 22, 3, 9, 5, -1, 2, 40, 6, 11],
+      [-1, 1, 2, 2, 3, 5, 6, 6, 9, 11, 22, 40],
+    ],
+    [[], []],
+  ])("should sort numbers in the array", (data, expected) => {
+    const result = bubbleSort(data);
 
-        expect(result).toEqual(expected)
-    })
-})
+    expect(result).toEqual(expected);
+  });
+});
