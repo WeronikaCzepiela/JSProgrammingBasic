b0b37dd91e52b3aa1c0890340375450ed079737e
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-04-20 23:04:45 +0200

    add new algorithms

---
+++ algorithms/valid-parentheses/valid-parentheses.js
@@ -0,0 +1,23 @@
+const validParentheses = (arg) => {
+  const argArray = Array.from(arg);
+  let firstValue;
+  let secondValue;
+  for (let i = 0; i < arg.length - 1; i = i + 2) {
+    firstValue = argArray[i];
+    secondValue = argArray[i + 1];
+    if (firstValue === "(") {
+      if (secondValue !== ")") return false;
+    }
+    if (firstValue === "[") {
+      if (secondValue !== "]") return false;
+    }
+    if (firstValue === "{") {
+      if (secondValue !== "}") return false;
+    }
+  }
+  return true;
+};
+
+console.log(validParentheses("()"));
+console.log(validParentheses("()[]{}"));
+console.log(validParentheses("(]"));
